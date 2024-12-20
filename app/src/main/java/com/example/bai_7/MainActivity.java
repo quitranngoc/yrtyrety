d4a034750ebdf87137f8027b0a4403f84595b34b
quitranngoc <qtran19052004@gmail.com>
2024-12-21 04:54:08 +0700

    MMMMMMMM

---
+++ app/src/main/java/com/example/bai_7/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.bai_7;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Spinner spnCategory;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        spnCategory = findViewById(R.id.spnCategory);
+
+        // Create a list of items for the spinner
+        List<String> list = new ArrayList<>();
+        list.add("Java");
+        list.add("Android");
+        list.add("PHP");
+        list.add("C#");
+        list.add("ASP.NET");
+
+        // Set up the adapter for the spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, list);
+        adapter.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
+        spnCategory.setAdapter(adapter);
+
+        // Set up item selection listener for the spinner
+        spnCategory.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, spnCategory.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                // Handle case where nothing is selected
+            }
+        });
+    }
+}
